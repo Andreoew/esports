c1a176e0323925e1a8b35e4eeab658123ba37393
André de Souza <andreoew@hotmail.com>
2022-09-19 00:21:31 -0300

    add project

---
+++ README.md
@@ -0,0 +1,40 @@
+# Project Esports  Games
+
+## Project web end mobile com backend
+
+# Backend tecnologies 
+  * Node
+  * Express
+  * Prisma
+  * Cors
+# Web tecnologies
+  * React 
+  * Typescript
+  * Vite
+  * tailwind 
+  * Radix
+  * Phosphor-react
+  * Axios
+# Mobile tecnologies
+  * React Native
+  * Phosphor-react
+  * expo-google-fonts/inter
+  * react-navigation/native
+  * react-navigation/native-stack
+  * expo: ~46.0.9,
+  * expo-clipboard
+  * expo-font: ~10.2.0
+  * expo-linear-gradient
+  * expo-modules-core
+  * expo-notifications
+  * expo-status-bar
+  * phosphor-react-native 
+  * react-native-safe-area-context
+  * react-native-screens
+  * react-native-svg
+  * react-native-web
+
+
+<span>Here you can see the project</span>
+
+<img src="esports.png">
---
+++ mobile/App.tsx
@@ -0,0 +1,65 @@
+import { useRef, useEffect } from 'react'
+import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+import { Subscription } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications';
+
+import { Routes } from './src/routes'
+import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
+
+import './src/services/notificationsConfig';
+import { getNotificationToken } from './src/services/getPushNotificationToken';
+
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+  const getNotificationListerner = useRef<Subscription>();
+  const responseNotificationListerner = useRef<Subscription>();
+
+  useEffect(() => {
+    getNotificationToken();
+  });
+
+  useEffect(() => {
+    getNotificationListerner.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    });
+
+    responseNotificationListerner.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    });
+
+    return () => {
+      if(getNotificationListerner.current && responseNotificationListerner.current ){
+        Notifications.removeNotificationSubscription(getNotificationListerner.current);
+        Notifications.removeNotificationSubscription(responseNotificationListerner.current);
+      }
+    }
+  }, []);
+
+
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </Background>
+  );
+}
